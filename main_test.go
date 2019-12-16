b023951b2dd3d78d00b5d7f941a4b940d3b59f58
178inaba <178inaba@users.noreply.github.com>
2019-12-17 00:55:53 +0900

    Add test

--- main_test.go
+++ main_test.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+const dstFilepath = "_testdata/dst.jpg"
+
+func TestRun(t *testing.T) {
+	var buf bytes.Buffer
+	if err := run("test", &buf); err != nil {
+		t.Fatalf("Should not be fail: %v.", err)
+	}
+
+	if os.Getenv("IS_CREATE_DST_FILE") == "true" {
+		createDstFile(t, buf.Bytes())
+	}
+
+	dstFile, err := os.Open(dstFilepath)
+	if err != nil {
+		t.Fatalf("Should not be fail: %v.", err)
+	}
+	want, err := ioutil.ReadAll(dstFile)
+	if err != nil {
+		t.Fatalf("Should not be fail: %v.", err)
+	}
+
+	if got := buf.Bytes(); !bytes.Equal(got, want) {
+		t.Fatalf("Should be same as %s.", dstFilepath)
+	}
+}
+
+func createDstFile(t *testing.T, bs []byte) {
+	t.Helper()
+
+	f, err := os.Create(dstFilepath)
+	if err != nil {
+		t.Fatalf("Should not be fail: %v.", err)
+	}
+	defer f.Close()
+
+	if _, err := f.Write(bs); err != nil {
+		t.Fatalf("Should not be fail: %v.", err)
+	}
+
+	t.Skip("Create destination file.")
+}
